4761730910114fea172d3c4517bc971cbea18b66
taissa <taissated@gmail.com>
2021-05-19 18:39:33 -0300

    Removido os comentários do fonte.

--- estrutura-enquanto.js
+++ estrutura-enquanto.js
@@ -1,26 +1,3 @@
-/*
-   nome: caractere
-   idade, limite, contador: inteiro
-   
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   escreva("Digite a quantidade de vezes que vai ser verificado a idade: ")
-   leia(limite)
-   contador := 0
-   enquanto contador < limite faca
-    escreva("Digite o nome da pessoa: ")
-    leia(nome)
-    escreva("Digite a idade do ", nome, ": ")
-    leia(idade)
-    se idade > 18 entao
-    escreval(nome, " Você é maior de idade!")
-   senao
-     escreval (nome, " Você é menor de idade!")
-   fimse
-   contador := contador + 1
- fimenquanto
-*/
 
 function acaoBotao(params) {
     var nome, idade, limite, contador
